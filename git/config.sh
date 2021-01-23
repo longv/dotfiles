9206cf268776afae1b0647923675531c1c888f03
longv <longvh96@gmail.com>
2021-01-23 22:37:12 +0200

    Add git config.sh

---
+++ git/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.name "longv"
+
+# COMMAND
+git config --global alias.stt status
+git config --global alias.logo "log --oneline --decorate"
+git config --global alias.ci commit
+git config --global alias.amend ci --amend
