a97f42a7bd6c014d12ecf5924e159a55a22baaa1
Long Vu <longv@longvs-mbp.lan>
2024-03-21 11:17:59 +0200

    Clean up git config

--- git/config.sh
+++ git/config.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 git config --global user.name "longv"
-
-# COMMAND
-git config --global --replace-all alias.stt status
-git config --global --replace-all alias.logo "log --oneline --decorate"
-git config --global --replace-all alias.ci commit
-git config --global --replace-all alias.amend "ci --amend"
